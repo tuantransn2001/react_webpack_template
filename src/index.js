ad7b58efc782198bcf63a5e62ec1606db753c0e1
PetuEx <petuexuaf@gmail.com>
2022-08-28 13:12:02 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <>Hello This is demo initial React project with webpack</>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
